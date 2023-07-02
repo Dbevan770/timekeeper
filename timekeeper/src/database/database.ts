38a60c5f298d3ca6ea2b49e7dfbbf77a0237a3dd
Daniel Bevan <daniel.j.bevan96@gmail.com>
2023-07-02 21:26:38 +0200

    Finished the AddWages form and included logic to display the total earnings upon submit. Added the ability to reset the form if the amount doesn't look correct. Started with writing the logic for the database operations.

---
+++ timekeeper/src/database/database.ts
@@ -0,0 +1,6 @@
+import { FIREBASE_AUTH } from "../firebaseConfig";
+import { FIREBASE_DB } from "../firebaseConfig";
+
+const auth = FIREBASE_AUTH;
+
+export const CreateWage = () => {};
--- timekeeper/src/scenes/Dashboard/AddWages/AddWages.tsx
+++ timekeeper/src/scenes/Dashboard/AddWages/AddWages.tsx
@@ -1,9 +1,61 @@
 import WageForm from "../../../components/WageForm/WageForm";
+import { useState } from "react";
+import { Typography, Link, Button } from "@mui/material";
+import "./AddWages.css";
 
 const AddWages = () => {
+  const [submitted, setSubmitted] = useState<boolean>(false);
+  const [currency, setCurrency] = useState<string>("");
+  const [totalEarned, setTotalEarned] = useState<string>("");
+
+  const handleSetSubmit = (
+    rate: string,
+    currency: string,
+    startHour: string,
+    startMin: string,
+    startMeridian: string,
+    endHour: string,
+    endMin: string,
+    endMeridian: string,
+    totalEarned: string,
+    breaks: string[]
+  ) => {
+    setSubmitted(true);
+    setCurrency(currency);
+    setTotalEarned(totalEarned);
+  };
+
+  const handleResetForm = () => {
+    setSubmitted(false);
+    setCurrency("");
+    setTotalEarned("");
+  };
   return (
     <div className="add-wages-container">
-      <WageForm />
+      {!submitted ? (
+        <WageForm onSubmit={handleSetSubmit} />
+      ) : (
+        <div className="earned-wages-container">
+          <Typography
+            variant="h6"
+            sx={{ fontWeight: "300", color: "rgba(255,255,255,0.48)" }}
+          >
+            Congrats, you earned
+          </Typography>
+          <Typography variant="h1">
+            {currency === "EUR" ? "€" + totalEarned : "$" + totalEarned}
+          </Typography>
+          <Typography variant="body1" sx={{ marginBottom: "1.5rem" }}>
+            Not right?{" "}
+            <Link onClick={handleResetForm} underline="none" color="secondary">
+              Try again
+            </Link>
+          </Typography>
+          <Button variant="contained" sx={{ width: "100%", fontSize: "1rem" }}>
+            Save
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
