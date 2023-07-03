9b5117082e0be4af14803cac4d124e467974874e
Daniel Bevan <daniel.j.bevan96@gmail.com>
2023-07-04 00:16:08 +0200

    Commented out unfinished features for deployment of App version 0.1.2

--- timekeeper/src/scenes/Dashboard/Dashboard.tsx
+++ timekeeper/src/scenes/Dashboard/Dashboard.tsx
@@ -3,14 +3,14 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Typography, Button, Link } from "@mui/material";
 import { Add } from "@mui/icons-material";
-import { GetWages, WageObjectProps } from "../../database/database";
+// import { GetWages, WageObjectProps } from "../../database/database";
 import SignOut from "../../auth/signout";
 import Loading from "../../components/Loading/Loading";
 import "./Dashboard.css";
 
 const Dashboard = () => {
   const [loading, setLoading] = useState<boolean>(false);
-  const [wages, setWages] = useState<WageObjectProps[]>([]);
+  // const [wages, setWages] = useState<WageObjectProps[]>([]);
   const { user } = useAuthContext();
   const navigate = useNavigate();
 
@@ -39,18 +39,18 @@ const Dashboard = () => {
     setLoading(false);
   }, [user]);
 
-  useEffect(() => {
-    const getWages = async () => {
-      let result: WageObjectProps[] = [];
-      if (user) {
-        result = await GetWages(user);
-      }
+  // useEffect(() => {
+  //   const getWages = async () => {
+  //     let result: WageObjectProps[] = [];
+  //     if (user) {
+  //       result = await GetWages(user);
+  //     }
 
-      setWages(result);
-    };
+  //     setWages(result);
+  //   };
 
-    getWages();
-  }, [user]);
+  //   getWages();
+  // }, [user]);
 
   return (
     <>
@@ -60,35 +60,35 @@ const Dashboard = () => {
             Hello {user && user.displayName}!
           </Typography>
           <div className="shift-container">
-            {wages.length > 0 ? (
+            {/* {wages.length > 0 ? (
               wages.map((wage, index) => (
                 <div className="wage-item" key={index}>
                   {wage.totalEarned}
                   {wage.shiftDate}
                 </div>
               ))
-            ) : (
-              <>
-                <Typography
-                  variant="h5"
-                  sx={{
-                    textWrap: "balance",
-                    color: "rgba(255,255,255,0.48)",
-                    marginBottom: "2rem",
-                  }}
-                >
-                  Hmm, there's nothing here!
-                </Typography>
-                <Button
-                  variant="contained"
-                  startIcon={<Add />}
-                  onClick={handleClick}
-                  sx={{ fontSize: "1rem", verticalAlign: "middle" }}
-                >
-                  Add Wages
-                </Button>
-              </>
-            )}
+            ) : ( */}
+            {/* <> */}
+            <Typography
+              variant="h5"
+              sx={{
+                textWrap: "balance",
+                color: "rgba(255,255,255,0.48)",
+                marginBottom: "2rem",
+              }}
+            >
+              Hmm, there's nothing here!
+            </Typography>
+            <Button
+              variant="contained"
+              startIcon={<Add />}
+              onClick={handleClick}
+              sx={{ fontSize: "1rem", verticalAlign: "middle" }}
+            >
+              Add Wages
+            </Button>
+            {/* </>
+            )} */}
           </div>
           <Link color="secondary" onClick={handleSignOut} underline="none">
             Sign Out
