a0bbd1b29bcb1e541072913515266aed1d1845a0
Daniel Bevan <daniel.j.bevan96@gmail.com>
2023-07-04 19:00:58 +0200

    Fixed some input validation issues with the time inputs. Added some vistual flair to the dashboard.

--- timekeeper/src/components/TimeInput/HourInput/HourInput.tsx
+++ timekeeper/src/components/TimeInput/HourInput/HourInput.tsx
@@ -18,7 +18,7 @@ const HourInput = ({ hour, setHour }: HourInputProps) => {
     } else if (intValue > 12 || intValue < 0 || value.length > 2) {
       return;
     } else {
-      if (value.length === 2 && value[1] === "0") {
+      if (value.length === 2 && value[1] === "0" && value[0] !== "1") {
         setter(value.slice(1));
       } else {
         setter(value);
